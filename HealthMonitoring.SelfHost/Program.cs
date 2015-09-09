01fe06fdc77ffa2d12d6e667b7a0af8a8a2d8a6d
wojtek <wojciech.kotlarski@wonga.com>
2015-09-09 14:02:02 +0100

    Basic project structure

---
+++ HealthMonitoring.SelfHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoring.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
