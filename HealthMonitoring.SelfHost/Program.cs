a74eea2250adee5d160bd04d694109c421574bf8
wojtek <wojciech.kotlarski@wonga.com>
2015-09-10 17:10:54 +0100

    UKPE-3 added logging to SelfHost

--- HealthMonitoring.SelfHost/Program.cs
+++ HealthMonitoring.SelfHost/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.Threading;
+using Common.Logging;
 using HealthMonitoring.SelfHost.Configuration;
 using Microsoft.Owin.Hosting;
 
@@ -8,14 +9,23 @@ namespace HealthMonitoring.SelfHost
 {
     public class Program
     {
+        private static readonly ILog Logger = LogManager.GetLogger<Program>();
         public static void Main(params string[] args)
         {
             string baseAddress = ConfigurationManager.AppSettings["BaseUrl"];
 
-            using (WebApp.Start<Startup>(baseAddress))
+            try
             {
-                Console.WriteLine("Started...");
-                Thread.Sleep(Timeout.InfiniteTimeSpan);
+                Logger.Info("Starting service...");
+                using (WebApp.Start<Startup>(baseAddress))
+                {
+                    Logger.Info("Started service...");
+                    Thread.Sleep(Timeout.InfiniteTimeSpan);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.FatalFormat("Service has terminated unexpectedly:\n{0}", e);
             }
         }
     }
