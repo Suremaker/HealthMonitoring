8f623f2ce95fbface5fcc57681f97e88324edf5c
wojtek <wojciech.kotlarski@wonga.com>
2015-09-18 12:02:23 +0100

    UKPE-3 added make

--- make/Makefile.ps1
+++ make/Makefile.ps1
@@ -1,11 +1,14 @@
-Define-Step -Name 'Step one' -Target 'build' -Body {
+Define-Step -Name 'Building' -Target 'build' -Body {
+	call "$($Context.NuGetExe)" restore HealthMonitoring.sln
 	call "msbuild.exe" HealthMonitoring.sln /t:"Clean,Build" /p:Configuration=Release /m /verbosity:m /nologo /p:TreatWarningsAsErrors=true
 }
 
-Define-Step -Name 'Step two' -Target 'build,deploy' -Body {
+Define-Step -Name 'Testing' -Target 'build,deploy' -Body {
+	. (require 'psmake.mod.testing')
+	
 	$tests = @()
 	$tests += Define-XUnitTests -GroupName 'Unit tests' -TestAssembly "*\bin\Release\*.UnitTests.dll"
 	$tests += Define-XUnitTests -GroupName 'Acceptance tests' -TestAssembly "*\bin\Release\*.AcceptanceTests.dll"
 	
-	$tests | Run-Tests -EraseReportDirectory -Cover -CodeFilter '+[HealthMonitoring*]* -[*Tests*]*' -TestFilter '*Tests.dll' | Generate-CoverageSummary | Check-AcceptableCoverage -AcceptableCoverage 95
+	$tests | Run-Tests -EraseReportDirectory -Cover -CodeFilter '+[HealthMonitoring*]* -[*Tests*]*' -TestFilter '*Tests.dll' | Generate-CoverageSummary | Check-AcceptableCoverage -AcceptableCoverage 90
 }
