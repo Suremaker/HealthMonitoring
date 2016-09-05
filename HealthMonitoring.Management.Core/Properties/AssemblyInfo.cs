55f46512debadde8faff0f84fb6f94720f00c810
wojtek <wojciech.kotlarski@wonga.com>
2016-09-05 11:12:54 +0100

    Updated Home page css to remove styles from changed behaviour (filter by status) Fixed bug with tag filter navigation

--- HealthMonitoring.Management.Core/Properties/AssemblyInfo.cs
+++ HealthMonitoring.Management.Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ddeac82f-7d78-4962-b180-9711d574af8d")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyFileVersion("2.2.1.0")]
