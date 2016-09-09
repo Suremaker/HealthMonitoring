909a6cc1e802e7a946c5dfe5e623dc38910196a3
Wojtek <wojciech.kotlarski@gmail.com>
2016-09-09 16:57:28 +0100

    Restored single thread test execution for monitors.core.unittests

--- HealthMonitoring.Monitors.Core.UnitTests/Properties/AssemblyInfo.cs
+++ HealthMonitoring.Monitors.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using Xunit;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
