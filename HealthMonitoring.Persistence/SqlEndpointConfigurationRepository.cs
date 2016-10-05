31aacb90cc8a9735bff998a832136f2d88ec13dc
wojtek <wojciech.kotlarski@wonga.com>
2016-10-05 17:54:09 +0100

    UKPE-1235 Initial implementation of EndpointUpdateFrequencyGuard

--- HealthMonitoring.Persistence/SqlEndpointConfigurationRepository.cs
+++ HealthMonitoring.Persistence/SqlEndpointConfigurationRepository.cs
@@ -6,16 +6,19 @@ using HealthMonitoring.Management.Core;
 using HealthMonitoring.Management.Core.Repositories;
 using HealthMonitoring.Model;
 using HealthMonitoring.Persistence.Entities;
+using HealthMonitoring.TimeManagement;
 
 namespace HealthMonitoring.Persistence
 {
     public class SqlEndpointConfigurationRepository : IEndpointConfigurationRepository
     {
         private readonly MySqlDatabase _db;
+        private readonly ITimeCoordinator _timeCoordinator;
 
-        public SqlEndpointConfigurationRepository(MySqlDatabase db)
+        public SqlEndpointConfigurationRepository(MySqlDatabase db, ITimeCoordinator timeCoordinator)
         {
             _db = db;
+            _timeCoordinator = timeCoordinator;
         }
 
         public void SaveEndpoint(Endpoint endpoint)
@@ -55,7 +58,7 @@ namespace HealthMonitoring.Persistence
         {
             using (var conn = _db.OpenConnection())
                 return conn.Query<EndpointEntity>("select * from EndpointConfig")
-                    .Select(e => new Endpoint(new EndpointIdentity(e.Id, e.MonitorType, e.Address), new EndpointMetadata(e.Name, e.GroupName, e.Tags.FromDbString())))
+                    .Select(e => new Endpoint(_timeCoordinator, new EndpointIdentity(e.Id, e.MonitorType, e.Address), new EndpointMetadata(e.Name, e.GroupName, e.Tags.FromDbString())))
                     .ToArray();
         }
     }
