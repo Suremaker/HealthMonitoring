6e673e68acd7f37ab7fc4c3b7c0d7ffb8f68bcde
wojtek <wojciech.kotlarski@wonga.com>
2016-10-19 17:40:17 +0100

    Added nuspec for HealthMonitoring.Integration.PushClient

--- HealthMonitoring.Integration.PushClient/Properties/AssemblyInfo.cs
+++ HealthMonitoring.Integration.PushClient/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HealthMonitoring.Integration.PushClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HealthMonitoring push monitor type integration client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wonga")]
 [assembly: AssemblyProduct("HealthMonitoring.Integration.PushClient")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Wonga")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
