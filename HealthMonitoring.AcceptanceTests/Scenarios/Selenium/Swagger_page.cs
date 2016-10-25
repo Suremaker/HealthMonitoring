245927f8b6ac9bc183e4d91320fad371222cbaa0
pawlyha <woonderluck@gmail.com>
2016-10-25 15:43:26 +0300

    PE-1233: Add manual basic authentication to swagger ui.

---
+++ HealthMonitoring.AcceptanceTests/Scenarios/Selenium/Swagger_page.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using HealthMonitoring.AcceptanceTests.Helpers;
+using LightBDD;
+
+namespace HealthMonitoring.AcceptanceTests.Scenarios.Selenium
+{
+    [FeatureDescription(
+@"In order to use the api
+As User 
+I want to open swagger page")]
+    public partial class Swagger_page
+    {
+        [Scenario]
+        public void Verification_of_basic_authentication_with_valid_credentials()
+        {
+            Runner.RunScenario(
+                _ => Given_swagger_page(),
+                _ => Given_endpoint_with_password_is_registered("registered", "http://localhost:2595/status", "group", MonitorTypes.HttpJson, _credentials.PersonalCredentials.Password),
+                _ => Given_endpoint_id_is_received(),
+                _ => When_user_expand_DELETE_endpoint_section(),
+                _ => When_user_fills_auth_form(_credentials.PersonalCredentials),
+                _ => When_user_enters_endpoint_id_parameter(),
+                _ => When_user_sends_request(),
+                _ => Then_response_status_should_be(HttpStatusCode.OK)
+                );
+        }
+
+        [Scenario]
+        public void Verification_of_basic_authentication_with_wrong_credentials()
+        {
+            Runner.RunScenario(
+                _ => Given_swagger_page(),
+                _ => Given_endpoint_with_password_is_registered("registered", "http://localhost:2595/status", "group", MonitorTypes.HttpJson, _credentials.MonitorCredentials.Password),
+                _ => Given_endpoint_id_is_received(),
+                _ => When_user_expand_DELETE_endpoint_section(),
+                _ => When_user_fills_auth_form(_credentials.MonitorCredentials),
+                _ => When_user_enters_endpoint_id_parameter(),
+                _ => When_user_sends_request(),
+                _ => Then_response_status_should_be(HttpStatusCode.Forbidden)
+                );
+        }
+    }
+}
---
+++ HealthMonitoring.SelfHost/Content/Swagger/basic-auth-extension.js
@@ -0,0 +1,73 @@
+﻿$(function () {
+    var authForm = "<form class='auth-form' style='display:none' title='Fill basic auth credentials if needed.'>" +
+            "<label style='margin-left: 10%'>basic auth: </label>" +
+            "<input placeholder='id' style='width: 25%;margin: 5px 0 0 10px;' name='cred_id' class='cred-id' />" +
+            "<input placeholder='password' style='width: 25%;margin: 5px 0 0 15px;' name='cred_pwd' class='cred-pwd' />" +
+        "</form>";
+
+    var placeToInsert = ".endpoint .heading > h3";
+    var url = ".endpoint .heading .path > a";
+    var headings = ".endpoint .heading";
+    var credentials = {
+        id: null,
+        password: null,
+        valid: function () {
+            return this.id != null && this.password != null;
+        }
+    };
+
+    insertCredentialsForm(placeToInsert);
+    setTimeout(showAuthForms, 1000);
+
+    function insertCredentialsForm(selector) {
+        if ($(selector) == null)
+            throw "could not insert basic auth form. element not exist:" + selector;
+
+        $(authForm).insertAfter(selector);
+    }
+
+    function authorizeHeader() {
+        if (credentials.valid()) {
+            var value = "Basic " + window.btoa(credentials.id + ":" + credentials.password);
+            var authKeyHeader = new SwaggerClient.ApiKeyAuthorization("Authorization", value, "header");
+            window.swaggerUi.api.clientAuthorizations.add("Authorization", authKeyHeader);
+        }
+    }
+
+    function showAuthForms() {
+        $(headings).each(function (i, heading) {
+            if ($(heading).next(".content").css('display') === 'block')
+                $(heading).children(".auth-form:first").show();
+        });
+    }
+
+    function updateCredentials(form) {
+        credentials.id = $(form).children("input[name=cred_id]:first").val();
+        credentials.password = $(form).children("input[name=cred_pwd]:first").val();
+        console.log(credentials);
+        authorizeHeader();
+    }
+
+    function toggleAuthForm(heading) {
+        var form = $(heading).children(".auth-form:first");
+        if ($(heading).next(".content").css('display') === 'block') {
+            form.hide();
+        } else {
+            form.show();
+            updateCredentials(form);
+        }
+    }
+
+    $(url).on("click", function () {
+        var heading = $(this).closest("div");
+        toggleAuthForm(heading);
+    });
+
+    $("input[name=cred_id]").on("change paste keyup", function () {
+        updateCredentials($(this).closest('.auth-form'));
+    });
+
+    $("input[name=cred_pwd]").on("change paste keyup", function () {
+        updateCredentials($(this).closest('.auth-form'));
+    });
+});
