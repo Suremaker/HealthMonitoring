7b043157cd0d7c3136ee8dd893ea02c13b91ef55
Wojtek <wojciech.kotlarski@gmail.com>
2016-11-22 10:55:46 +0000

    Update AssemblyInfo.cs

--- HealthMonitoring.Monitors.Core.UnitTests/Properties/AssemblyInfo.cs
+++ HealthMonitoring.Monitors.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -1,1 +1,15 @@
-﻿
+using System.Reflection;
+using System.Runtime.InteropServices;		
+		
+[assembly: AssemblyTitle("HealthMonitoring.Monitors.Core.UnitTests")]		
+[assembly: AssemblyDescription("")]		
+[assembly: AssemblyConfiguration("")]		
+[assembly: AssemblyCompany("")]		
+[assembly: AssemblyProduct("HealthMonitoring.Monitors.Core.UnitTests")]		
+[assembly: AssemblyCopyright("Copyright © Wonga")]		
+[assembly: AssemblyTrademark("")]		
+[assembly: AssemblyCulture("")]		
+[assembly: ComVisible(false)]		
+[assembly: Guid("cb4eb4ad-dcfa-406a-8a06-51a1e31cda81")]		
+[assembly: AssemblyVersion("3.5.1.0")]		
+[assembly: AssemblyFileVersion("3.5.1.0")]
