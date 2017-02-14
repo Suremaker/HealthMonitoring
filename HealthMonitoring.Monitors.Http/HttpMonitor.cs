d200685940cb249404cd134992cc9fa3d762c556
wojtek <wojciech.kotlarski@wonga.com>
2017-02-14 14:22:10 +0000

    Updated HttpMonitor to not follow redirects

--- HealthMonitoring.Monitors.Http/HttpMonitor.cs
+++ HealthMonitoring.Monitors.Http/HttpMonitor.cs
@@ -24,21 +24,24 @@ namespace HealthMonitoring.Monitors.Http
 
         public async Task<HealthInfo> CheckHealthAsync(string address, CancellationToken cancellationToken)
         {
-            using (var client = new HttpClient())
+            using (var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = false }))
             using (var response = await client.GetAsync(address, cancellationToken))
             {
-                switch (response.StatusCode)
-                {
-                    case HttpStatusCode.NotFound:
-                        return new HealthInfo(HealthStatus.NotExists);
-                    case HttpStatusCode.ServiceUnavailable:
-                        return new HealthInfo(HealthStatus.Offline);
-                    case HttpStatusCode.OK:
-                        return new HealthInfo(HealthStatus.Healthy, await ReadSuccessfulContent(response.Content));
-                    default:
-                        return new HealthInfo(HealthStatus.Faulty, await GetFaultyResponseDetails(response));
-                }
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                    return new HealthInfo(HealthStatus.NotExists);
+                if (response.StatusCode == HttpStatusCode.ServiceUnavailable)
+                    return new HealthInfo(HealthStatus.Offline);
+                if (response.IsSuccessStatusCode)
+                    return new HealthInfo(HealthStatus.Healthy, await ReadSuccessfulContent(response.Content));
+                if(IsRedirect(response.StatusCode))
+                    return new HealthInfo(HealthStatus.Healthy, await GetResponseDetails(response));
+                return new HealthInfo(HealthStatus.Faulty, await GetResponseDetails(response));
             }
+        }
+
+        private static bool IsRedirect(HttpStatusCode statusCode)
+        {
+            return (int) statusCode >= 300 && (int) statusCode < 400;
         }
 
         protected virtual Task<IReadOnlyDictionary<string, string>> ReadSuccessfulContent(HttpContent content)
@@ -47,12 +50,12 @@ namespace HealthMonitoring.Monitors.Http
             return Task.FromResult(result);
         }
 
-        private async Task<IReadOnlyDictionary<string, string>> GetFaultyResponseDetails(HttpResponseMessage response)
+        private async Task<IReadOnlyDictionary<string, string>> GetResponseDetails(HttpResponseMessage response)
         {
             return new Dictionary<string, string>
             {
-                {"code",response.StatusCode.ToString()},
-                {"content",await response.Content.ReadAsStringAsync()}
+                {"code", response.StatusCode.ToString()},
+                {"content", await response.Content.ReadAsStringAsync()}
             };
         }
     }
