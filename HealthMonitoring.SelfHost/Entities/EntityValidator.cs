2eab80bd2c0921d6df5ce08864c11c7dd6c6c713
DanielKerri-WDFC <daniel.kerri@wonga.com>
2017-03-13 15:30:24 +0000

    tags validator shim while developing has been removed

--- HealthMonitoring.SelfHost/Entities/EntityValidator.cs
+++ HealthMonitoring.SelfHost/Entities/EntityValidator.cs
@@ -18,16 +18,14 @@ namespace HealthMonitoring.SelfHost.Entities
     {
         public static ValidationResult CheckForUnallowedSymbols(this string[] tags)
         {
-            return ValidationResult.Success;
+            const string allowedSymbols = "_";
 
-            //const string allowedSymbols = "_";
+            if (tags == null || tags.All(tag => tag.All(symbol => char.IsLetterOrDigit(symbol) || allowedSymbols.Contains(symbol))))
+            {
+                return ValidationResult.Success;
+            }
 
-            //if (tags == null || tags.All(tag => tag.All(symbol => char.IsLetterOrDigit(symbol) || allowedSymbols.Contains(symbol))))
-            //{
-            //    return ValidationResult.Success;
-            //}
-
-            //throw new ArgumentException("Tags contains unallowed symbols.");
+            throw new ArgumentException("Tags contains unallowed symbols.");
         }
     }
 }
