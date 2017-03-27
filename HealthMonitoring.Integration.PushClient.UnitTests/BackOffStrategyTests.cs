f4f2db4f0089136b1301719fff2e1825a5f6becf
DanielKerri-WDFC <daniel.kerri@wonga.com>
2017-03-27 11:27:18 +0100

    Most up to date changes (#1)
    * Updated to allow client bespoke back off strategy
    
    * Completed refactoring and added tests
    
    * Minor Version applied on Client
    
    * BackOffPlan ctor and private settings
    
    * PE1670, filters added, tests added
    
    * removing attributes
    
    * PE1670 case compare
    
    * Removed publish default option
    
    * pe1670 formatting
    
    * pe1670 layout/formatting
    
    * layout/formatting and passing tests
    
    * init for task
    
    * main task completed
    
    * Acceptance tests completed
    
    * tags validator shim while developing has been removed
    
    * Removed unused styling
    
    * simplifying css
    
    * statuses set i n order of importance
    
    * build cleanup; to allow AppVeyor to pass tests
    
    * pe 1669 remove unused method
    
    * pe 1669 cleaning solution (with make local) to allow app veyor to pass
    
    * pe 1669 some css reduce
    
    * manually removed config files
    
    * PE 1669 Cleaner Refactoring Solution
    
    * PE 1669 Updating Selenium and related Packages due to a Chrome update to v57
    
    * PE 1669 Removing obsolete ImplicitWait methods
    
    * PE 1754 & PE 1706 Tests are failing due to time outs; and not just packages

---
+++ HealthMonitoring.Integration.PushClient.UnitTests/BackOffStrategyTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HealthMonitoring.Integration.PushClient.Helpers;
+using Moq;
+using Xunit;
+
+namespace HealthMonitoring.Integration.PushClient.UnitTests
+{
+    public class BackOffStrategyTests
+    {
+
+
+        [Theory]
+        [InlineData(null, 1D)]
+        [InlineData(1D, 2D)]
+        [InlineData(2D, 4D)]
+        [InlineData(16D, 32D)]
+        [InlineData(120D, 120D)]
+        [InlineData(121D, 120D)]
+        public async Task RecommendedBackOffStrategy_Returns_Expectected_NextTimeInterval(double? currentInterval, double? nextInterval)
+        {
+            TimeSpan? currentTimeInterval = currentInterval.HasValue
+                ? TimeSpan.FromSeconds(currentInterval.Value)
+                : (TimeSpan?) null;
+
+            var result = await new RecommendedBackOffStrategy().NextInterval(currentTimeInterval, It.IsAny<CancellationToken>());
+            
+            Assert.Equal(nextInterval, result.Value.TotalSeconds);
+        }
+
+        [Theory]
+        [InlineData(null, true)]
+        [InlineData(1D, true)]
+        [InlineData(2D, true)]
+        [InlineData(16D, true)]
+        [InlineData(120D, true)]
+        [InlineData(121D, true)]
+        public async Task RecommendedBackOffStrategy_Returns_Expectected_ShouldLogStatus(double? currentInterval, bool shouldLog)
+        {
+            TimeSpan? currentTimeInterval = currentInterval.HasValue
+                ? TimeSpan.FromSeconds(currentInterval.Value)
+                : (TimeSpan?)null;
+
+            var result = await new RecommendedBackOffStrategy().RecordLog(currentTimeInterval, It.IsAny<CancellationToken>());
+
+            Assert.Equal(result, shouldLog);
+        }
+
+        [Theory]
+        [InlineData(null, 1D, true)]
+        [InlineData(1D, 2D, true)]
+        [InlineData(2D, 4D, true)]
+        [InlineData(16D, 32D, true)]
+        [InlineData(120D, 120D, true)]
+        [InlineData(121D, 120D, true)]
+        public async Task RecommendedBackOffStrategy_Returns_Expectected_BackOffPlan(double? currentInterval, double nextInterval, bool shouldLog)
+        {
+            TimeSpan? currentTimeInterval = currentInterval.HasValue
+                ? TimeSpan.FromSeconds(currentInterval.Value)
+                : (TimeSpan?)null;
+
+            var result = await new RecommendedBackOffStrategy().GetCurrent(currentTimeInterval, It.IsAny<CancellationToken>());
+
+            Assert.NotNull(result);
+            Assert.Equal(result.ShouldLog, shouldLog);
+            Assert.Equal(result.RetryInterval, TimeSpan.FromSeconds(nextInterval));
+        }
+    }
+}
---
+++ HealthMonitoring.Integration.PushClient/Helpers/RecommendedBackOffStrategy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HealthMonitoring.TimeManagement;
+
+namespace HealthMonitoring.Integration.PushClient.Helpers
+{
+    public class RecommendedBackOffStrategy : IBackOffStategy
+    {
+        private readonly TimeSpan _maxRepeatDelay;
+
+        private readonly TimeSpan _intervalMultiplier;
+
+        private readonly TimeSpan _initialInterval = TimeSpan.FromSeconds(1);
+
+        public RecommendedBackOffStrategy()
+            : this(TimeSpan.FromSeconds(120), TimeSpan.FromSeconds(2))
+        {
+        }
+
+        public RecommendedBackOffStrategy(TimeSpan maxRepeatDelay, TimeSpan intervalMultiplier)
+        {
+            _maxRepeatDelay = maxRepeatDelay;
+            _intervalMultiplier = intervalMultiplier;
+        }
+        
+
+        public virtual Task<TimeSpan?> NextInterval(TimeSpan? currentInterval, CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                var suggestedInterval = currentInterval?.TotalSeconds * _intervalMultiplier.TotalSeconds ?? _initialInterval.TotalSeconds;
+
+                TimeSpan newInterval = TimeSpan.FromSeconds(Math.Min(_maxRepeatDelay.TotalSeconds, suggestedInterval));
+                
+                return Task.FromResult(newInterval as TimeSpan?);
+            }
+
+            return Task.FromResult<TimeSpan?>(null);
+        }
+
+
+        public virtual Task<bool> RecordLog(TimeSpan? currentInterval, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(true);
+        }
+
+        public virtual async Task<BackOffPlan> GetCurrent(TimeSpan? currentInterval, CancellationToken cancellationToken)
+        {
+            TimeSpan? newInterval = await NextInterval(currentInterval, cancellationToken);
+
+            bool shouldLog = await RecordLog(currentInterval, cancellationToken);
+
+            var backOffPlan = new BackOffPlan(newInterval, shouldLog);
+
+            return backOffPlan;
+        }
+    }
+}
